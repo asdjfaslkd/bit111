9651076b99a8f814dd480a962c4cd0c8f7a309b4
姚凯 <2514015219@qq.com>
2025-11-25 15:12:46 +0000

    update my_string/Test.cpp.
    Signed-off-by: 姚凯 <2514015219@qq.com>

--- my_string/Test.cpp
+++ my_string/Test.cpp
@@ -1,20 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-
-#include<iostream>
+#include <iostream>
 using namespace std;
-#include"string.h"
+#include <string>
+#include "string.h"
 
 int main()
 {
-	/*int i = 1234;
-	double d = 11.22;
-	string s1 = to_string(i);
-	string s2 = to_string(d);
-
-	string s3("45.55");
-	double d3 = stod(s3);*/
-
-	bit::Test_string4();
-
+	bit::Test_string5();
 	return 0;
 }
